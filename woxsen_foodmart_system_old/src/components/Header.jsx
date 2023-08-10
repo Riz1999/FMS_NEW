70244cd4db3c7c938fd0f015811f9b6751cd6196
Riya Gupta <98754072+riyagupta2011@users.noreply.github.com>
2023-08-10 23:12:34 +0530

    added logout

--- woxsen_foodmart_system_old/src/components/Header.jsx
+++ woxsen_foodmart_system_old/src/components/Header.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import "./Header.css";
 import menu from "./menu.svg";
-const Header = () => {
+
+const Header = ({ isLoggedIn, onLogout }) => {
   return (
     <header>
       <nav className="header__nav">
         <div className="header__logo">
-          {/* <h4 data-aos="fade-down"><span>EnviroFeast</span>  : Fostering Sustainable Dining</h4> */}
-
-          <div className="header__logo-overlay"></div>
+        {/* <h4 data-aos="fade-down"><span>EnviroFeast</span>  : Fostering Sustainable Dining</h4> */}
         </div>
 
-        <ul className="header__menu" data-aos="fade-down">
+        <ul className="header__menu">
           <li>
             <a href="#food">Menu</a>
           </li>
-
           <li>
             <a href="#about">About Us</a>
           </li>
+          {isLoggedIn && (
+            <li>
+             <button className="logout-button" onClick={onLogout}>Logout</button>
+            </li>
+          )}
         </ul>
 
         <ul className="header__menu-mobile" data-aos="fade-down">
